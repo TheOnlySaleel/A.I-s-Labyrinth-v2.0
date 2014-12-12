c0f046a6693d0707b755461e8e1839d2deca85d9
Tyler Roach <tyler.roach93@yahoo.com>
2014-12-12 14:16:36 -0800

    Adding Old Textures and Scripts

---
+++ Scripts/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public int nextLevel; //Number of the next scene/level to load;
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+		Application.LoadLevel(nextLevel);
+	}
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public int speed; //Change speed in Inspector
+
+	// Using FixedUpdate over Update for Physics calculations
+	void FixedUpdate () {
+				float moveHorizontal = Input.GetAxis ("Horizontal");
+				float moveVertical = Input.GetAxis ("Vertical");
+				//Player only moves on two axes.
+				Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+				//Multiplying by time makes speed independent of framerate.
+				rigidbody.velocity = movement * Time.deltaTime * speed;
+		}
+}
